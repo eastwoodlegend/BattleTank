7ecfb6a3a3a4d6251bf650ddf7482c2ddcd73546
eastwoodlegend <eastwoodlegend@gmail.com>
2019-09-30 22:33:40 +0100

    no message

---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+
+
+UCLASS()
+class BATTLETAN_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+
+};
