b685c7fea79b42d7637285692fba451daa176984
eastwoodlegend <eastwoodlegend@gmail.com>
2019-10-03 23:01:29 +0100

    tidy up

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -1,8 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Engine/World.h"
-#include "Public/Projectile.h"
-#include "Public/TankBarrel.h"
 #include "public/Tank.h"
 
 // Sets default values
